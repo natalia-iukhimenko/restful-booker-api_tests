fffe09387b55490a3ab4c60b6380b9a8ed98418e
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2020-08-14 21:33:28 +0300

    Added new endpoints

--- src/main/java/ru/iukhimenko/restfulbooker/Endpoints.java
+++ src/main/java/ru/iukhimenko/restfulbooker/Endpoints.java
@@ -1,4 +1,7 @@
+package ru.iukhimenko.restfulbooker;
+
 public final class Endpoints {
     public static final String bookingParameterized = "/booking/{id}";
     public static final String booking = "/booking";
+    public static final String auth = "/auth";
 }

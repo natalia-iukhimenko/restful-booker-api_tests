e572484f08f30142ca506b3c4e16879b9c17b61d
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2020-08-14 22:43:33 +0300

    Added DELETE method tests

---
+++ src/test/java/ru/iukhimenko/restfulbooker/apitests/DeleteBookingTest.java
@@ -0,0 +1,59 @@
+package ru.iukhimenko.restfulbooker.apitests;
+
+import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import ru.iukhimenko.restfulbooker.ApiTest;
+import ru.iukhimenko.restfulbooker.Endpoints;
+import ru.iukhimenko.restfulbooker.BookingDataProvider;
+import ru.iukhimenko.restfulbooker.api.BookingApi;
+import ru.iukhimenko.restfulbooker.dto.booking.BookingDTO;
+import static io.restassured.RestAssured.given;
+import static ru.iukhimenko.restfulbooker.requestspecs.BookingRequestSpecs.withIdPathParam;
+
+public class DeleteBookingTest extends ApiTest {
+    private BookingDTO testBookingDTO;
+
+    @BeforeMethod
+    public void createTestBooking() {
+        testBookingDTO = BookingDataProvider.getBookingDTOWithAllValues();
+        Integer createdBookingId = BookingApi.postBooking(testBookingDTO);
+        testBookingDTO.setId(createdBookingId);
+    }
+
+    @Test
+    public void canNotDeleteWithoutCookieOrAuthorization() {
+        given().spec(withIdPathParam(testBookingDTO.getId()))
+                .when().delete(Endpoints.bookingParameterized)
+                .then().assertThat().statusCode(HttpStatus.SC_FORBIDDEN);
+    }
+
+    @Parameters( {"username", "password"} )
+    @Test
+    public void canDeleteWithValidCookie(String username, String password) {
+        String tokenValue = BookingApi.getToken(username, password);
+        given().spec(withIdPathParam(testBookingDTO.getId())).cookie("token", tokenValue)
+                .when().delete(Endpoints.bookingParameterized)
+                .then().statusCode(HttpStatus.SC_CREATED);
+        // check that no id
+    }
+
+    @Parameters( {"username", "password"} )
+    @Test
+    public void canDeleteWithAuthorizationHeader(String username, String password) {
+        given().spec(withIdPathParam(testBookingDTO.getId())).auth().preemptive().basic(username, password)
+                .when().delete(Endpoints.bookingParameterized)
+                .then().statusCode(HttpStatus.SC_CREATED);
+        // check that no id
+    }
+
+    @Parameters( {"username", "password"} )
+    @Test
+    public void notAllowedToDeleteNotExisting(String username, String password) {
+        BookingDTO updatedBooking = BookingDataProvider.getBookingDTOWithAllValues();
+        given().spec(withIdPathParam(-1)).auth().preemptive().basic(username, password)
+                .when().delete(Endpoints.bookingParameterized)
+                .then().assertThat().statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    }
+}
